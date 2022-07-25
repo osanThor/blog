4dbf696d63db0b865cf9172fb75aa4f09ede243c
junyoung <jadw9715@gmail.com>
2022-07-26 00:37:34 +0900

    add mongoose

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+//비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT } = process.env;
 
 const api = require('./api');
 
@@ -16,6 +20,8 @@ app.use(bodyParser());
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Litening to port 4000');
+//PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Litening to port %d', port);
 });
