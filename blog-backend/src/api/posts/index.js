95d475f27e7be635ded45d6501c69e3d289e627a
junyoung <jadw9715@gmail.com>
2022-08-02 23:07:47 +0900

    post API check objectId

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -5,8 +5,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router();
+post.get('/:id', postsCtrl.read);
+post.delete('/:id', postsCtrl.remove);
+post.patch('/:id', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
