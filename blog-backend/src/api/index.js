1d025ad820d101bc8afef642010fbe5c314086df
junyoung <jadw9715@gmail.com>
2022-08-08 00:18:26 +0900

    make auth API

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 import posts from './posts';
+import auth from './auth';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 //router을 내보냅니다.
 export default api;
