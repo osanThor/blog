8769221a5dcfacf34e1a3a6356c4961d999e3f0f
junyoung <jadw9715@gmail.com>
2022-08-08 23:36:35 +0900

    create auth service

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -4,13 +4,23 @@ import Joi from '@hapi/joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400;
     return;
   }
-  return next();
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const write = async (ctx) => {
@@ -33,6 +43,7 @@ export const write = async (ctx) => {
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
   try {
     await post.save();
@@ -49,10 +60,19 @@ export const list = async (ctx) => {
     ctx.status = 400;
     return;
   }
+  const { tag, username } = ctx.query;
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
 
   try {
-    const posts = await Post.find().sort({ _id: -1 }).limit(10).lean().exec();
-    const postCount = await Post.countDocuments().exec();
+    const posts = await Post.find(query)
+      .sort({ _id: -1 })
+      .limit(10)
+      .lean()
+      .exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map((post) => ({
       ...post,
@@ -65,17 +85,7 @@ export const list = async (ctx) => {
 };
 
 export const read = async (ctx) => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404;
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.trhow(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
 
 export const remove = async (ctx) => {
@@ -117,4 +127,13 @@ export const update = async (ctx) => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
 };
