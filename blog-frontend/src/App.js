34939cc23a59e82a8b03db060a6d578620f4710a
junyoung <jadw9715@gmail.com>
2022-08-11 22:48:38 +0900

    react router 연결

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/loginPage';
+import PostListPage from './pages/postListPage';
+import PostPage from './pages/postPage';
+import RegisterPage from './pages/registerPage';
+import WritePage from './pages/writePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route element={<PostListPage />} path="/" exact />
+        <Route element={<PostListPage />} path="/@:username" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<RegisterPage />} path="/register" />
+        <Route element={<WritePage />} path="/write" />
+        <Route element={<PostPage />} path="/@:username/:postId" />
+      </Routes>
+    </>
   );
 }
 
---
+++ blog-frontend/src/pages/writePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WritePage = () => {
+  return <div>글쓰기 페이지</div>;
+};
+
+export default WritePage;
