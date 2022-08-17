969116dedf6a891c0a47c01c032c7a6c62f712da
junyoung <jadw9715@gmail.com>
2022-08-17 23:42:12 +0900

    add tagbox in page

--- blog-frontend/src/pages/writePage.js
+++ blog-frontend/src/pages/writePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Responsive from '../components/common/responsive';
 import Editor from '../components/write/editor';
+import TagBox from '../components/write/tagBox';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
+      <TagBox />
     </Responsive>
   );
 };
