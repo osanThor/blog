470fd3737c20f0fd007e0310b27a127dfca6f0e9
junyoung <jadw9715@gmail.com>
2022-08-18 23:53:37 +0900

    add write Button

--- blog-frontend/src/pages/writePage.js
+++ blog-frontend/src/pages/writePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Responsive from '../components/common/responsive';
 import Editor from '../components/write/editor';
 import TagBox from '../components/write/tagBox';
+import WriteActionButtons from '../components/write/writeActionButtons';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteActionButtons />
     </Responsive>
   );
 };
