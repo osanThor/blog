f0cb4f56891a369e5dc9a5cdfaec0b77c43cf968
junyoung <jadw9715@gmail.com>
2022-08-26 01:34:34 +0900

    백엔드에서 리액트 정적 처리

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -31,6 +34,14 @@ app.use(jwtMiddleware);
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 //PORT가 지정되어 있지 않다면 4000을 사용
 const port = PORT || 4000;
