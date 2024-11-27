c6caad74207443931bb862a86c125f2de9024f25
junyoung <jadw9715@gmail.com>
2024-11-28 02:41:13 +0900

    test. search page 최적화

--- src/app/search/[[...slug]]/page.tsx
+++ src/app/search/[[...slug]]/page.tsx
@@ -1,11 +1,22 @@
 import BigTitle from "@/components/common/BigTitle";
 import PostsGridContainer from "@/containers/posts/PostsGridContainer";
-import { getSearchPosts } from "@/services/posts.service";
-import { PageProps } from "../../../../.next/types/app/page";
-
-export default async function SearchPage({ params }: PageProps) {
+import { getAllPosts } from "@/services/posts.service";
+export const dynamic = "force-static";
+export async function generateStaticParams() {
+  return [];
+}
+type Props = {
+  params: Promise<{ slug?: string[] }>;
+};
+export default async function SearchPage({ params }: Props) {
   const { slug } = await params;
-  const list = await getSearchPosts(slug);
+  const list = await getAllPosts().then((list) =>
+    slug
+      ? list.filter((item) =>
+          item.title.toLowerCase().includes(slug[0].toLowerCase())
+        )
+      : list
+  );
   return (
     <>
       <BigTitle text={slug ? decodeURI(slug[0]) : "Search"} />
