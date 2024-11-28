7c493e6c9bc7a1495a1e70296150ca14d07b86c8
junyoung <jadw9715@gmail.com>
2024-11-28 14:40:13 +0900

    Refactor. post service 코드 정리

--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -16,18 +16,34 @@ export type Frontmatter = {
   featured?: boolean;
 };
 export type PostItem = Frontmatter & { href: string };
+export type SeriesItem = {
+  name: string;
+  count: number;
+};
+export type TagItem = { name: string; count: number };
 
 // 파일을 검색할 기본 경로 설정
 const baseDir = path.join(process.cwd(), "src/posts");
 
-export const getPost = async (
-  filename: string[],
+// 공통 유틸리티 함수
+const readMDXFile = async (filePath: string) => {
+  try {
+    return await fs.readFile(filePath, "utf-8");
+  } catch (error) {
+    console.error(`Failed to read file "${filePath}":`, error);
+    throw error;
+  }
+};
+
+// MDX 파일 컴파일
+const compileMDXFile = async (
+  filePath: string,
   mdxOptions?: Omit<CompileOptions, "outputFormat" | "providerImportSource"> & {
-    useDynamicImport?: boolean | undefined;
+    useDynamicImport?: boolean;
   },
   components?: React.ComponentProps<typeof MDXProvider>["components"]
 ) => {
-  const content = await fs.readFile(path.join(baseDir, ...filename), "utf-8");
+  const content = await readMDXFile(filePath);
   return await compileMDX<Frontmatter>({
     source: content,
     options: {
@@ -38,104 +54,104 @@ export const getPost = async (
   });
 };
 
+// 단일 포스트 가져오기
+export const getPost = async (
+  filename: string[],
+  mdxOptions?: CompileOptions,
+  components?: React.ComponentProps<typeof MDXProvider>["components"]
+) => {
+  const filePath = path.join(baseDir, ...filename);
+  return compileMDXFile(filePath, mdxOptions, components);
+};
+
+// 카테고리별 포스트 가져오기
 export const getPostsByCategory = cache(
-  async (folder: string): Promise<PostItem[]> => {
-    const folderPath = path.join(baseDir, folder);
+  async (category: string): Promise<PostItem[]> => {
+    const folderPath = path.join(baseDir, category);
     try {
-      const files = await fs
-        .readdir(folderPath)
-        .then((files) => files.filter((file) => path.extname(file) === ".mdx"));
-      return await Promise.all(
-        files.map(async (filename) => {
-          const formatFileName = filename.replace(".mdx", "");
-          const { frontmatter } = await getPost([folder, filename]);
+      const files = await fs.readdir(folderPath);
+      const mdxFiles = files.filter((file) => path.extname(file) === ".mdx");
+
+      const posts = await Promise.all(
+        mdxFiles.map(async (filename) => {
+          const filePath = path.join(folderPath, filename);
+          const { frontmatter } = await compileMDXFile(filePath);
           return {
-            href: formatFileName,
+            href: filename.replace(".mdx", ""),
             ...frontmatter,
           };
         })
-      ).then((arr) =>
-        arr.sort(
-          (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-        )
+      );
+
+      return posts.sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
       );
     } catch (error) {
-      console.error(`Error reading folder "${folder}":`, error);
+      console.error(`Error fetching posts for category "${category}":`, error);
       return [];
     }
   }
 );
 
+// 모든 포스트 가져오기
 export const getAllPosts = cache(async (): Promise<PostItem[]> => {
-  const [devPosts, lifePosts] = await Promise.all([
-    getPostsByCategory("dev"),
-    getPostsByCategory("life"),
-  ]);
-
-  return [...devPosts, ...lifePosts].sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  );
+  const categories = ["dev", "life"];
+  const postsByCategory = await Promise.all(categories.map(getPostsByCategory));
+  return postsByCategory
+    .flat()
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 });
 
-export type SeriesItem = {
-  name: string;
-  count: number;
-};
-
+// 시리즈 데이터 가져오기
 export const getAllSeries = async (folder?: string): Promise<SeriesItem[]> => {
-  const seriesMap = new Map<string, number>();
-  const list = folder ? await getPostsByCategory(folder) : await getAllPosts();
-  list
-    .filter((item) => !!item.series)
-    .forEach((item) => {
-      if (item.series)
-        seriesMap.set(item.series, (seriesMap.get(item.series) || 0) + 1);
-    });
-  return Array.from(seriesMap.entries())
+  const posts = folder ? await getPostsByCategory(folder) : await getAllPosts();
+  const seriesCount = posts.reduce((map, { series }) => {
+    if (series) map.set(series, (map.get(series) || 0) + 1);
+    return map;
+  }, new Map<string, number>());
+
+  return Array.from(seriesCount.entries())
     .map(([name, count]) => ({ name, count }))
     .sort((a, b) => b.count - a.count);
 };
 
-export const getSeries = async (name?: string) => {
+// 특정 시리즈 가져오기
+export const getSeries = async (name?: string): Promise<PostItem[]> => {
   if (!name) return [];
-  return await getAllPosts().then((data) =>
-    data.filter((item) => item.series && item.series === name)
-  );
+  const posts = await getAllPosts();
+  return posts.filter((post) => post.series === name);
 };
 
-export type TagItem = { name: string; count: number };
-
+// 태그 데이터 가져오기
 export const getAllTags = async (): Promise<TagItem[]> => {
-  const tagMap = new Map<string, number>();
-  const list = await getAllPosts();
+  const posts = await getAllPosts();
+  const tagCount = posts.reduce((map, { tags }) => {
+    tags.forEach((tag) => map.set(tag, (map.get(tag) || 0) + 1));
+    return map;
+  }, new Map<string, number>());
 
-  list.forEach((item) => {
-    item.tags.forEach((tag) => {
-      tagMap.set(tag, (tagMap.get(tag) || 0) + 1);
-    });
-  });
-  return Array.from(tagMap.entries())
+  return Array.from(tagCount.entries())
     .map(([name, count]) => ({ name, count }))
     .sort((a, b) => b.count - a.count);
 };
 
-export const getPostsByTag = async (name: string) => {
-  return await getAllPosts().then((data) =>
-    data.filter((item) => item.tags.includes(name))
-  );
+// 특정 태그 포스트 가져오기
+export const getPostsByTag = async (tag: string): Promise<PostItem[]> => {
+  const posts = await getAllPosts();
+  return posts.filter((post) => post.tags.includes(tag));
 };
 
-export const getFeaturedPost = async () => {
-  return await getAllPosts().then((data) =>
-    data.filter((item) => item.featured === true).slice(0, 4)
-  );
+// 추천 포스트 가져오기
+export const getFeaturedPost = async (): Promise<PostItem[]> => {
+  const posts = await getAllPosts();
+  return posts.filter((post) => post.featured).slice(0, 4);
 };
 
+// 타이틀 검색 가져오기
 export const getSearchPosts = async (keyword: string) => {
-  if (!keyword) return await getAllPosts();
-  return await getAllPosts().then((data) =>
-    data.filter((item) =>
-      item.title.toLowerCase().includes(keyword.toLowerCase())
-    )
+  const list = await getAllPosts();
+  if (!keyword) return list;
+  return list.filter((item) =>
+    item.title.toLowerCase().includes(keyword.toLowerCase())
   );
 };
