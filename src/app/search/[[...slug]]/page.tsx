efb41ba5437b8a419506aeeb509b1ddc571e2733
junyoung <jadw9715@gmail.com>
2024-11-28 11:18:10 +0900

    test. search page static

--- src/app/search/[[...slug]]/page.tsx
+++ src/app/search/[[...slug]]/page.tsx
@@ -1,12 +1,13 @@
 import BigTitle from "@/components/common/BigTitle";
 import SearchListContainer from "@/containers/search/SearchListContainer";
-export const dynamicParams = false;
+export const dynamicParams = true;
+export async function generateStaticParams() {
+  return [{ slug: [""] }];
+}
+
 type Props = {
   params: Promise<{ slug?: string[] }>;
 };
-export async function generateStaticParams() {
-  return [];
-}
 
 export default async function SearchPage({ params }: Props) {
   const { slug } = await params;
