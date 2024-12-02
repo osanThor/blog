587ca487f4fb4fef8832f75c6b1447ab73b1a90c
junyoung <jadw9715@gmail.com>
2024-12-03 01:03:51 +0900

    Add. 11월 회고

--- next.config.mjs
+++ next.config.mjs
@@ -18,8 +18,6 @@ const config = {
   // next config here...
   images: {
     formats: ["image/avif", "image/webp"],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 350],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   swcMinify: true,
   experimental: {
