88ecf33799d35aa287003423d3ab493a486d4b5c
junyoung <jadw9715@gmail.com>
2024-12-03 02:06:12 +0900

    fix. build error

--- src/components/common/SlideCard.tsx
+++ src/components/common/SlideCard.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { PropsWithChildren } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCreative } from "swiper/modules";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 import { Post } from "#site/content";
-
-import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/effect-creative";
