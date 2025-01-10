f102c59b6e14064c97f2cb90a3eb4731ed0a6198
junyoung <jadw9715@gmail.com>
2025-01-10 11:57:09 +0900

    chore. static build

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,13 +2,13 @@ import { getAllPosts } from "@/services/posts.service.velite";
 import { getMetadata } from "@/utils/getMetadata";
 import SearchContainer from "@/components/search/SearchContainer";
 import { Suspense } from "react";
-
+export const dynamic = "force-static";
 type Props = {
   searchParams: Promise<{ query?: string }>;
 };
 
 export async function generateMetadata({ searchParams }: Props) {
-  const { query } = await searchParams;
+  const query = (await searchParams).query;
   return getMetadata({
     title: `검색 결과 ${query || ""}`,
   });
