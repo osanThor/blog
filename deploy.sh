f01a88430455bb5ac3b8ffc6c4daa633e5804d9e
junyoung <jadw9715@gmail.com>
2025-01-13 23:33:57 +0900

    chore. aws

--- deploy.sh
+++ deploy.sh
@@ -25,14 +25,14 @@ elif [ "${DEPLOYMENT_GROUP_NAME}" == "til_fe_dev" ]; then
   echo "개발 서버 배포"
   cd "${REPOSITORY}"
   sudo npm install
-  pm2 describe til-dev > /dev/null
+  pm2 describe given-log > /dev/null
   if [ $? -eq 0 ]; then
 	  # 실행 중인 경우
-	  echo "til-dev 프로세스가 실행 중입니다."
+	  echo "given-log 프로세스가 실행 중입니다."
 	  sudo npm run pm2:reload:dev
   else
   	# 실행 중이 아닌 경우
-  	echo "til-dev 프로세스가 실행되지 않았습니다."
+  	echo "given-log 프로세스가 실행되지 않았습니다."
 	  sudo npm run pm2:start:dev
   fi
 fi
