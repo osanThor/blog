5a654f014b4b38c7e4ec31aab895ce8cd8565be8
junyoung <jadw9715@gmail.com>
2025-01-13 23:39:41 +0900

    chore. ci/cd

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,20 @@ REPOSITORY_PROD=/home/ubuntu/deploy
 
 echo "DEPLOYMENT_GROUP_NAME: ${DEPLOYMENT_GROUP_NAME}"
 
-if [ "${DEPLOYMENT_GROUP_NAME}" == "til_fe_prod" ]; then
+if [ "${DEPLOYMENT_GROUP_NAME}" == "given-log" ]; then
   echo "운영 서버 배포"
   cd "${REPOSITORY_PROD}"
   
   # production 환경인 경우에 대한 처리
   sudo npm install
-  pm2 describe til-product > /dev/null
+  pm2 describe given-log > /dev/null
   if [ $? -eq 0 ]; then
 	  # 실행 중인 경우
-	  echo "til-product 프로세스가 실행 중입니다."
+	  echo "given-log 프로세스가 실행 중입니다."
 	  sudo npm run pm2:reload:prod
   else
   	# 실행 중이 아닌 경우
-  	echo "til-product 프로세스가 실행되지 않았습니다."
+  	echo "given-log 프로세스가 실행되지 않았습니다."
 	  sudo npm run pm2:start:prod
   fi
 elif [ "${DEPLOYMENT_GROUP_NAME}" == "til_fe_dev" ]; then
